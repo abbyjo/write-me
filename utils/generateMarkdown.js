15a9ee792bf75654d5c869941d469a2966ee5f5e
abbyjo <abbyjo225@gmail.com>
2023-08-29 20:32:12 -0400

    Edited license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (data.license === "None") {return ""
-} else if (data.license === "MIT") {return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-} else if (data.license === "Apache 2.0") {return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-} else if (data.license === "ISC") {return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
-} else if (data.license === "MPL 2.0") {return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  if (license === "None") {return ""
+} else if (license === "MIT") {return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+} else if (license === "Apache 2.0") {return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+} else if (license === "ISC") {return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+} else if (license === "MPL 2.0") {return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
 }
 
 }
@@ -18,7 +18,7 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (data.license === "None") {return ""
-} else {return `This project is covered under the ${data.license} license.`}
+} else {return `This project is covered under the ${license} license.`}
 
 }
 
